c26b611817173561d8a20a35bc487996ae101868
Chris Manson <chris@manson.ie>
2015-07-02 12:59:38 +0100

    2 working tests 14 pending

--- test/fixtures/routes/mongo.js
+++ test/fixtures/routes/mongo.js
@@ -4,11 +4,38 @@ function success(req, res){
     return res.send("Success");
 }
 
+function getSession(req, res, next) {
+        if(!req.headers.authorization){
+            return res.status(401).send("Missing authorization header");
+        }
+
+        var accessToken = req.headers.authorization.split(/\s+/).pop();
+
+        //verify the access-token
+        return authmakerVerify.mongo(accessToken)
+            .then(function(oauthSession){
+                req.oauthSession = oauthSession;
+
+                next();
+            })
+            .then(null, function(err){
+
+                if (err.message.indexOf("Not Authorized") >= 0) {
+                    res.status(401);
+                } else {
+                    res.status(500);
+                }
+
+                return res.send(err.message);
+            });
+    }
+
+
 module.exports.autoroute = {
     get: {
         '/noverify': success,
-        '/verify': [authmakerVerify.mongo],
+        '/verify': [getSession],
         '/jointrated': [authmakerVerify.mongoRateLimited],
-        '/splitrated': []
+        '/splitrated': [authmakerVerify.mongo, authmakerVerify.rateLimited]
     }
 };
--- test/routes/mongo.js
+++ test/routes/mongo.js
@@ -2,27 +2,47 @@ var expect = require('chai').expect;
 var request = require('supertest');
 var autoroute = require('express-autoroute');
 
-var routeFile = rootRequire('./test/fixtures/routes/mongo');
-
 describe("mongo verify functions", function() {
 
     before(function() {
-        //function type
-        // routeFile(global.app);
-
-        ////autoroute type
         autoroute(global.app, {
-           throwErrors: true,
-           routeFile: rootPath('test/fixtures/routes/mongo.js')
+            throwErrors: true,
+            routeFile: rootPath('test/fixtures/routes/mongo.js')
         });
     });
-
-
 
     it("should success every time with no verify", function(done) {
         request(global.app)
             .get('/noverify')
             .expect(200, done);
+    });
+
+    describe("verify only", function() {
+        it("should fail when there is no access token", function(done) {
+            request(global.app)
+                .get('/verify')
+                .expect(401)
+                .end(done);
+        });
+        it("should fail when there is an access token but it doesn't match the database");
+        it("should succeed when there is a valid access token");
+        it("should fail if the endpoint needs a scope permission but the session doesn't have any");
+        it("should fail if the endpoint needs a scope permission but the session has the wrong one");
+        it("should succeed if the endpoint needs a scope permission and it is on the session");
+    });
+
+    describe("rate limited", function() {
+        it("should fail if there is no scope and no default");
+        it("should succedd if there is a default scope");
+        it("should allow 5 requests for a scope that ends with _limit_5_days");
+        it("should allow 10 requests for a scope that ends with _limit_5_seconds where there is a 1 second gap after the first 5 requests");
+        it("should fail with 6 requests for a scope that ends with _limit_5_days");
+        it("should fail with 11 requests for a scope that ends with _limit_5_seconds where there is a 1 second gap after the first 5 requests");
+        it("should allow 2 users to send 5 requests each for a scope that ends with _limit_5_days");
+    });
+
+    describe("split rate limited", function() {
+        it("should allow you to split rate limiting and verification");
     });
 
     it.skip("should return an array of things", function(done) {
